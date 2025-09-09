ec455529ad8994c9acf65f0e0384e9baa6a1d30d
Celio Junior <celiojunior@ioasys.com.br>
2025-09-09 12:54:07 -0300

    Initial Commit

---
+++ ObjectDetection/ObjectDetectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  ObjectDetectionApp.swift
+//  ObjectDetection
+//
+//  Created by Celio Junior on 09/09/25.
+//
+
+import SwiftUI
+
+@main
+struct ObjectDetectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
